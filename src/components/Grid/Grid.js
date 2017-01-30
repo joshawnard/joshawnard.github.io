40dcee355b3faa1cc1637e884e22c993804c4e33
jard@widen.com <jard@widen.com>
2017-01-29 23:05:56 -0500

    wrote function to take in number of columns and generate a grid wrapper and item list

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -5,40 +5,48 @@ import styles from './Grid.scss';
 class Grid extends Component {
 
   render() {
+
+    // Takes number of columns and generates grid item wrapper and list
+    var gridItems = function(col) {
+      var cols = [];
+      for (var i = 1; i <= col; i++) {
+          cols.push(i);
+      }  
+      return (
+        <div className={'grid-wrapper col-' + col}>
+          { cols.map(function(col){
+             return <div className='grid-item'>{col}</div>;
+            })
+          }
+        </div>
+      ); 
+    };
+
     return (
       <div>
         <h2>CSS Grid</h2>
-        <div>
-          <h3>Basic Grid</h3>
-          <h4>3 columns</h4>
-          <div className='grid-wrapper col-3'>
-            <div className='grid-item'>a</div>
-            <div className='grid-item'>b</div>
-            <div className='grid-item'>c</div>
-          </div>
+        <h3>Basic Grid</h3>
 
-          <h4>4 columns</h4>
-          <div className='grid-wrapper col-4'>
-            <div className='grid-item'>a</div>
-            <div className='grid-item'>b</div>
-            <div className='grid-item'>c</div>
-            <div className='grid-item'>d</div>
-          </div>
+        <h4>Two Columns</h4>
+        {gridItems(2)}
 
-          <h4>5 columns</h4>
-          <div className='grid-wrapper col-5'>
-            <div className='grid-item'>a</div>
-            <div className='grid-item'>b</div>
-            <div className='grid-item'>c</div>
-            <div className='grid-item'>d</div>
-            <div className='grid-item'>e</div>      
-          </div>
-        </div>
+        <h4>Three Columns</h4>
+        {gridItems(3)}
 
+        <h4>Four Columns</h4>
+        {gridItems(4)}
+
+        <h4>Five Columns</h4>
+        {gridItems(5)}
+
+        <h4>Six Columns</h4>
+        {gridItems(6)}
+
+        <h4>Twelve Columns</h4>
+        {gridItems(12)}
       </div>
     );
   }
-
 }
 
 export default withStyles(Grid, styles)
